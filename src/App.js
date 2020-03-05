5e54d9c783e4a70a73eabe80cdfc40aa1d2f8e0a
Clem <clemence.fauchet23@gmail.com>
2020-03-05 18:59:00 +0100

    Username submit & layout link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import './styles/App.scss'
 import PopUp from './components/PopUp/PopUp'
--- src/components/PopUp/PopUp.js
+++ src/components/PopUp/PopUp.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
+import CompleteLayout from '../BasicLayout/CompleteLayout'
+
+export const UserContext = React.createContext()
 
 function PopUp() {
   const [isOpen, setIsOpen] = useState(false)
+  const [username, setUsername] = useState('')
+
+  const [loadingLayout, setLoadingLayout] = useState(false)
 
   const openingLetter = () => {
     setTimeout(() => {
@@ -9,21 +15,45 @@ function PopUp() {
     }, 2500)
   }
 
-  return (
-    <div class='container' onMouseMove={openingLetter}>
-      <div className={`letter ${isOpen ? 'open' : ''}`}>
-        <span className='cover'></span>
-        <h1>Welcome to Hogwarts</h1>
-        <h3>Who are you?</h3>
-        <label>
-          <input type='text' placeholder='username' />
-        </label>
-        <svg viewBox='0 0 30 30' width='30px'>
-          <path d='M0 0 L15 15 L0 30'></path>
-        </svg>
+  const handleUsername = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const submitUser = (e) => {
+    e.preventDefault()
+    setLoadingLayout(true)
+  }
+
+  if (loadingLayout) {
+    return (
+      <UserContext.Provider value={username}>
+        <CompleteLayout />
+      </UserContext.Provider>
+    )
+  } else {
+    return (
+      <div className='container' onMouseMove={openingLetter}>
+        <div className={`letter ${isOpen ? 'open' : ''}`}>
+          <span className='cover'></span>
+          <h1>Welcome to Hogwarts</h1>
+          <h3>Who are you?</h3>
+          <form onSubmit={submitUser}>
+            <label>
+              <input
+                type='text'
+                placeholder='username'
+                value={username}
+                onChange={handleUsername}
+              />
+            </label>
+          </form>
+          <svg viewBox='0 0 30 30' width='30px' onClick={submitUser}>
+            <path d='M0 0 L15 15 L0 30'></path>
+          </svg>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default PopUp
