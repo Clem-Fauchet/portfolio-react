47571e46dd221566fe2e281866d93a86c12a74c8
Clem <clemence.fauchet23@gmail.com>
2020-03-05 17:44:02 +0100

    Adding class to letter with new style

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 import './styles/App.scss'
 import PopUp from './components/PopUp/PopUp'
-import PopUpTwo from './components/PopUp/PopUpTwo'
 
 function App() {
   return (
     <div className='App'>
-      {/* <PopUp /> */}
-      <PopUpTwo />
+      <PopUp />
     </div>
   )
 }
--- src/components/PopUp/PopUp.js
+++ src/components/PopUp/PopUp.js
@@ -6,6 +6,13 @@ function PopUp() {
       <div className='letter'>
         <span className='cover'></span>
         <h1>Welcome to Hogwarts</h1>
+        <h3>Who are you?</h3>
+        <label>
+          <input type='text' placeholder='username' />
+        </label>
+        <svg viewBox='0 0 30 30' width='30px'>
+          <path d='M0 0 L15 15 L0 30'></path>
+        </svg>
       </div>
     </>
   )
