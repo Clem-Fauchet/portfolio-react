cfa49d6bfe5c153c570b83aab508d14b86a92f25
Clem <clemence.fauchet23@gmail.com>
2020-03-12 16:59:31 +0100

    Updates

---
+++ precache-manifest.bee8bf43f24a5df77002d6f1a6201123.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c09f6e60da8d56917fe531f0fa26d0",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "0cbff54b96ccaea19ba6",
+    "url": "/portfolio-react/static/css/main.9cb410f4.chunk.css"
+  },
+  {
+    "revision": "afd778407f80f624c5d2",
+    "url": "/portfolio-react/static/js/2.6f3fee88.chunk.js"
+  },
+  {
+    "revision": "425f71bb909b45cb21829b79476852ea",
+    "url": "/portfolio-react/static/js/2.6f3fee88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cbff54b96ccaea19ba6",
+    "url": "/portfolio-react/static/js/main.8cbc8a27.chunk.js"
+  },
+  {
+    "revision": "6b5b75f90561b48ba189",
+    "url": "/portfolio-react/static/js/runtime-main.83698611.js"
+  },
+  {
+    "revision": "6372f0afc97c15f8ec06fa36a4885acf",
+    "url": "/portfolio-react/static/media/castle.6372f0af.svg"
+  },
+  {
+    "revision": "e050601b339eea303d01033bd49387e4",
+    "url": "/portfolio-react/static/media/cauldron.e050601b.svg"
+  },
+  {
+    "revision": "81417168f4f165561cac132bab51ca55",
+    "url": "/portfolio-react/static/media/chococard.81417168.svg"
+  },
+  {
+    "revision": "13c1ca521af70b6833cdf2cd65b16c89",
+    "url": "/portfolio-react/static/media/defense.13c1ca52.svg"
+  },
+  {
+    "revision": "0b3a66bd69d5039bcbeba23e5c0b33b6",
+    "url": "/portfolio-react/static/media/history.0b3a66bd.svg"
+  },
+  {
+    "revision": "2a678fe307f7bed0592f3e7079eaf312",
+    "url": "/portfolio-react/static/media/potion.2a678fe3.svg"
+  },
+  {
+    "revision": "632da2168c4e646b7b5c1a6986cc20ee",
+    "url": "/portfolio-react/static/media/times.632da216.svg"
+  },
+  {
+    "revision": "1091107305eb28472b9dc633974ec87b",
+    "url": "/portfolio-react/static/media/trap.10911073.svg"
+  },
+  {
+    "revision": "dd024f3576e5829651b3be34dae6dfa8",
+    "url": "/portfolio-react/static/media/wand.dd024f35.svg"
+  }
+]);
