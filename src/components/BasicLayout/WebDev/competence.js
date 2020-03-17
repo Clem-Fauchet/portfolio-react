2f95e592a9dbcaa059e337de76a6fa2070f436b4
Clem-Fauchet <cfauchet.pro@gmail.com>
2020-03-17 16:16:46 +0100

    Changing image path - fourth

--- src/components/BasicLayout/WebDev/competence.js
+++ src/components/BasicLayout/WebDev/competence.js
@@ -1,64 +1,64 @@
 export const competence = [
-  {
-    id: 1,
-    tool: 'HTML',
-    icon: '../src/assets/logoSkills/html.svg',
-    done: 90,
-  },
+	{
+		id: 1,
+		tool: 'HTML',
+		icon: '/static/media/html.svg',
+		done: 90,
+	},
 
-  {
-    id: 2,
-    tool: 'CSS',
-    icon: '../src/assets/logoSkills/css.svg',
-    done: 90,
-  },
+	{
+		id: 2,
+		tool: 'CSS',
+		icon: '/static/media/css.svg',
+		done: 90,
+	},
 
-  {
-    id: 3,
-    tool: 'Javascript',
-    icon: '../src/assets/logoSkills/javascript.svg',
-    done: 70,
-  },
+	{
+		id: 3,
+		tool: 'Javascript',
+		icon: '/static/media/javascript.svg',
+		done: 70,
+	},
 
-  {
-    id: 4,
-    tool: 'Bootstrap',
-    icon: '../src/assets/logoSkills/bootstrap.svg',
-    done: 58,
-  },
+	{
+		id: 4,
+		tool: 'Bootstrap',
+		icon: '/static/media/bootstrap.svg',
+		done: 58,
+	},
 
-  {
-    id: 5,
-    tool: 'React',
-    icon: '../src/assets/logoSkills/react.svg',
-    done: 78,
-  },
+	{
+		id: 5,
+		tool: 'React',
+		icon: '/static/media/react.svg',
+		done: 78,
+	},
 
-  {
-    id: 6,
-    tool: 'Wordpress',
-    icon: '../src/assets/logoSkills/wordpress.svg',
-    done: 70,
-  },
+	{
+		id: 6,
+		tool: 'Wordpress',
+		icon: '/static/media/wordpress.svg',
+		done: 70,
+	},
 
-  {
-    id: 7,
-    tool: 'Sass',
-    icon: '../src/assets/logoSkills/sass.svg',
-    done: 73,
-  },
+	{
+		id: 7,
+		tool: 'Sass',
+		icon: '/static/media/sass.svg',
+		done: 73,
+	},
 
-  {
-    id: 8,
-    tool: 'Git',
-    icon: '../src/assets/logoSkills/git.svg',
-    done: 60,
-  },
+	{
+		id: 8,
+		tool: 'Git',
+		icon: '/static/media/git.svg',
+		done: 60,
+	},
 
-  {
-    id: 9,
-    tool: 'Adobe',
-    icon: '../src/assets/logoSkills/adobe.svg',
-    done: 75,
-  },
+	{
+		id: 9,
+		tool: 'Adobe',
+		icon: '/static/media/adobe.svg',
+		done: 75,
+	},
 ]
